2448e799e8ca23d492ebb5663c98bc5ab7b2aed3
Amrit <amrittheengineer@users.noreply.github.com>
2020-04-25 03:17:00 +0530

    ClassAnnouncements Screen UI designed

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,23 +1,60 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonIcon,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
+import { home, search, filter } from "ionicons/icons";
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+      {/* <IonHeader>
         <IonToolbar>
           <IonTitle>Tab 1</IonTitle>
         </IonToolbar>
-      </IonHeader>
+      </IonHeader> */}
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <div className="page-container">
+          <div className="header">
+            <div className="tab-name-container">
+              <div className="icon">
+                <IonIcon icon={home} />
+              </div>
+              <div className="title">Announcements</div>
+              <div className="actions">
+                <IonIcon icon={filter} />
+                <IonIcon icon={search} />
+              </div>
+            </div>
+          </div>
+          <div className="body">
+            <div className="announcement-list">
+              <div className="school-card">
+                <div className="card-title">Science</div>
+                <div className="card-message">
+                  Tomorrow you have a test on Quantum Physics
+                </div>
+                <div className="author">2 hrs ago</div>
+              </div>
+              <div className="school-card">
+                <div className="card-title">Science</div>
+                <div className="card-message">
+                  Tomorrow you have a test on Quantum Physics Tomorrow you have
+                  a test on Quantum Physics Tomorrow you have a test on Quantum
+                  Physics Tomorrow you have a test on Quantum Physics Tomorrow
+                  you have a test on Quantum Physics
+                </div>
+                <div className="author">2 hrs ago</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
